bc5f20e41deb8defbfd33dadc433763b88f34a06
Angelo <angelorpt@gmail.com>
2019-12-22 13:26:02 -0400

    + Adicionando o component Grid

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
-  <div>
-    <header>
-      <nav-bar cor="green darken-1" url="#" logo="Social" />
-    </header>
+   <div>
+      <header>
+         <nav-bar cor="green darken-1" url="#" logo="Social" />
+      </header>
 
-    <main>
-      <div class="container">
-        <router-view/>
-      </div>
-    </main>
+      <main>
+         <div class="container">
+            <div class="row">
+               <Grid tamanho="4">
+                  Menu
+               </Grid>
+               <Grid tamanho="8">
+                  <router-view/>
+               </Grid>
+            </div>
+         </div>
+      </main>
 
-    <footer>
-      <rodape cor="green darken-1" ano="2019" logo="Social" descricao="Um descrição do site">
-        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>      
-      </rodape>
-    </footer>
-  </div>
+      <footer>
+         <rodape cor="green darken-1" ano="2019" logo="Social" descricao="Um descrição do site">
+         <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+         <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+         <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+         <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>      
+         </rodape>
+      </footer>
+   </div>
 </template>
 
 <script>
 
-  import NavBar from '@/components/layouts/NavBar'
-  import Rodape from '@/components/layouts/Rodape'
+   import NavBar from '@/components/layouts/NavBar'
+   import Rodape from '@/components/layouts/Rodape'
+   import Grid   from '@/components/layouts/Grid'
 
-  export default {
-    name: 'app',
-    components: {
-      NavBar, Rodape
-    }
-  }
+   export default {
+      name: 'app',
+      components: {
+         NavBar, Rodape, Grid
+      }
+   }
 
 </script>
 
