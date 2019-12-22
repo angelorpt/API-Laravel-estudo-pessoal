13c80d528254f3c0295d53a72fccfc79760a24d5
Angelo <angelorpt@gmail.com>
2019-12-22 13:08:12 -0400

    + Adicionando o Componente de Rodapé

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <div>
-    <nav-bar cor="green darken-1" url="#" logo="Social">
+    <header>
+      <nav-bar cor="green darken-1" url="#" logo="Social" />
+    </header>
 
-    </nav-bar>
-    <router-view/>
+    <main>
+      <div class="container">
+        <router-view/>
+      </div>
+    </main>
+
+    <footer>
+      <rodape cor="green darken-1" ano="2019" logo="Social" descricao="Um descrição do site">
+        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>      
+      </rodape>
+    </footer>
   </div>
 </template>
 
 <script>
 
   import NavBar from '@/components/layouts/NavBar'
+  import Rodape from '@/components/layouts/Rodape'
 
   export default {
     name: 'app',
     components: {
-      NavBar
+      NavBar, Rodape
     }
   }
-  
+
 </script>
 
 <style>
